14abfd62a567f7281727c3c4f65e8addb2e66c95
sanfen <a@sanfen.me>
2015-06-20 16:16:30 +0800

    看片神器,专为河南公务员设计

---
+++ function.php
@@ -0,0 +1,113 @@
+<?php 
+
+// 请求
+// http://hnwcf.59iedu.com/Home/Timing?
+// usrId=63735c7761074d838b05c95f5ee5346a &
+// medId=ba42ecce41e2458394d5299124806a31&
+// callback=jsonp1433817771187&
+// CurrentTimespan=53&
+// Id=97cbbe2b8b38496aafe2d09a7544c41b&
+// SscId=da33d7dac0394fce8341dcf4f9cbbeaf&
+// SstId=f1edff6455364540b20556d6b64d7528&
+// CourseSdlId=8c51fe48930a42b7a1bf680b91d4bd0f&
+// TrainSdlId=7e69e97329cc4deb9c5f319d0da2e953&
+// Type=2&
+// CurrentLength=30 ++自增
+
+//通过播放页面返回的html拼装需要请求的url
+function GetRequestUrlByHtml($html)
+{
+	if( trim($html) == "" ) {
+		echo "\n=======================\n";
+		echo "需要解析的html为空，返回";
+		return ;
+		echo "\n=======================\n";
+	}
+	
+	
+	$verifyUrl = "";
+
+	preg_match("/var timingUrl = (.*)(?=;)/", $html, $matches);
+
+	$matches = explode("\"", $matches[1]);
+	$verifyUrl .= $matches[1];		//url + usrId
+	$verifyUrl .= $matches[3];		//medId
+	//$verifyUrl .= "&callback=?";	//callback
+
+	preg_match("/CurrentTimespan:(.*)(?=,)/", $html, $matches);
+	$verifyUrl .= "&CurrentTimespan=" . trim($matches[1]);
+
+	preg_match("/TrainSdlId:(.*)(?=,)/", $html, $matches);
+	$verifyUrl .= "&TrainSdlId=" . trim($matches[1]);
+
+	preg_match("/Id:(.*)(?=,)/", $html, $matches);
+	$verifyUrl .= "&Id=" . trim($matches[1]);
+
+	preg_match("/SscId:(.*)(?=,)/", $html, $matches);
+	$verifyUrl .= "&SscId=" . trim($matches[1]);
+
+	preg_match("/SstId:(.*)(?=,)/", $html, $matches);
+	$verifyUrl .= "&SstId=" . trim($matches[1]);
+
+	preg_match("/CourseSdlId:(.*)(?=,)/", $html, $matches);
+	$verifyUrl .= "&CourseSdlId=" . trim($matches[1]);
+
+	preg_match("/TrainSdlId:(.*)(?=,)/", $html, $matches);
+	$verifyUrl .= "&TrainSdlId=" . trim($matches[1]);
+
+	$verifyUrl = str_replace("\"", "", $verifyUrl);
+
+	static $number = 0;
+	$verifyUrl .= "&Type=2";
+	$verifyUrl .="&CurrentLength=".$number++;
+
+
+	return $verifyUrl;
+}
+
+
+//支持跳转的curl
+function curl_redirect_exec($ch, &$redirects, $curlopt_returntransfer = false, $curlopt_maxredirs = 10, $curlopt_header = false) {
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$data = curl_exec($ch);
+
+	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	$exceeded_max_redirects = $curlopt_maxredirs > $redirects;
+	$exist_more_redirects = false;
+	if ($http_code == 301 || $http_code == 302) {
+		if ($exceeded_max_redirects) {
+			list($tmp, $header) = explode("\r\n\r\n", $data, 2);
+			$matches = array();
+			preg_match('/(Location:|URI:)(.*?)\n/', $header, $matches);
+			$url = trim(array_pop($matches));
+			$url_parsed = parse_url($url);
+			if (isset($url_parsed)) {
+
+				if(strpos($url,'http') === false){
+					echo $url = "http://xy.gkwlpx.com".$url;
+				}
+				curl_setopt($ch, CURLOPT_URL, $url);
+				$redirects++;
+				return curl_redirect_exec($ch, $redirects, $curlopt_returntransfer, $curlopt_maxredirs, $curlopt_header);
+			}
+		} else {
+			$exist_more_redirects = true;
+		}
+	}
+	if ($data !== false) {
+		if (!$curlopt_header)
+			list(,$data) = explode("\r\n\r\n", $data, 2);
+		if ($exist_more_redirects) return false;
+		if ($curlopt_returntransfer) {
+			return $data;
+		} else {
+			if (curl_errno($ch) === 0) return true;
+			else return false;
+		}
+	} else {
+		return false;
+	}
+}
+
+?>
---
+++ shenqi.php
@@ -0,0 +1,205 @@
+<?php
+include "PhpSimple/HtmlDomParser.php";
+include "function.php";
+
+
+$baseUrl = "http://xy.gkwlpx.com";
+
+$url = "http://hn.gkwlpx.com/Public/Control/GetValidateCode?time=".time().rand(100,999);
+$ch = curl_init();
+$cookie_jar = tempnam('./tmp','JSESSIONID');  
+//设置文件读取并提交的cookie路径  
+curl_setopt ( $ch, CURLOPT_COOKIESESSION, true);
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_jar);  
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+//打印获得的数据
+//print_r($output);
+
+
+//写入图片
+$fp=fopen("./verifyCode.jpg",'wb');
+fwrite($fp, $output);
+fclose($fp);
+
+
+echo "请查看图片并输入\n";
+$verify = trim(fgets(STDIN));	//接收用户输入  
+
+$loginAccount = "";
+$loginPassword = "";
+
+echo "请输入身份证号\n";
+$loginAccount = trim(fgets(STDIN));	//接收用户输入
+$loginPassword = substr($loginAccount, -6);
+echo "默认密码身份证后6位 : " . $loginPassword;
+
+//{{{ 登录
+$loginUrl = "http://hn.gkwlpx.com/Home/Login/HngkDoLoginByRgn?rgnId=411400";
+//参数
+$params = array("X-Requested-With"=>"XMLHttpRequest", "btnSubmit"=>"立即登录", "LoginType"=>0, "LoginAccount"=>$loginAccount, "LoginPassword"=>$loginPassword, "LoginValCode"=>$verify);
+
+curl_setopt($ch, CURLOPT_URL, $loginUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// post数据
+curl_setopt($ch, CURLOPT_POST, 1);
+// post的变量
+curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+$output = curl_exec($ch);
+echo $output;
+//}}}
+
+
+$homePage = "http://hn.gkwlpx.com/Home/RegionHome?rgnId=411400&tmp=".date("Y-m-d", time() );
+curl_setopt($ch, CURLOPT_URL, $homePage);
+$output= curl_exec($ch);
+
+
+//跳转
+$studentCenter = "http://hn.gkwlpx.com/Public/ShareSso/ToStudy";
+
+curl_setopt($ch, CURLOPT_URL, $studentCenter);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, true);
+
+
+$times = 0;
+$output = curl_redirect_exec($ch, $times, true);
+
+
+//进入课程页面
+$courseList = "http://xy.gkwlpx.com/Course/MyCourse/Index";
+curl_setopt($ch, CURLOPT_URL, $courseList);
+$output = curl_exec($ch);
+
+echo "\n==================================== \n";
+echo $output;
+echo "\n==================================== \n";
+
+
+
+use Sunra\PhpSimple\HtmlDomParser;
+
+//$str = file_get_contents("./a.html");
+
+$html = HtmlDomParser::str_get_html($output);
+$elems =  $html->find("div[id='tabsLearning'] table thead tr th a");
+//var_dump( count( $elems ) );
+
+$courseListUrl = array();
+foreach( $elems as $e ) {
+	//有两种链接，功能一样，过滤掉一种
+	// /Course/MyCourse/MediaList?sscId=a311526918bc412181872c2c3f6b7c5b
+	// /Course/MyCourse/CourseStudy?sscId=a311526918bc412181872c2c3f6b7c5b
+	if( strpos($e->href, "MediaList") !== false ) {
+		$courseListUrl[] = $baseUrl.$e->href;	//没有根地址，添加上
+	}
+}
+
+echo "============课程列表请求地址==========\n";
+var_dump($courseListUrl);
+
+
+echo "============课程播放页面列表==========\n";
+$coursePlayList = array();
+
+foreach( $courseListUrl as $c ) {
+	curl_setopt($ch, CURLOPT_URL, $c);
+	$output = curl_exec($ch);
+	$html = HtmlDomParser::str_get_html($output);
+	$elems =  $html->find("tr");
+	foreach($elems as $e) {
+		$html = HtmlDomParser::str_get_html($e->innertext);
+		$tmpStr = $html->find("embed", 0)->src;
+		list($_, $progress) = explode("=", $tmpStr);
+		if(intval($progress)==100) continue;	//进度满了，跳过
+		
+		$coursePlayList[] = str_replace("&amp;", "&", $baseUrl.$html->find("a",0)->href);		//过滤htmlencode
+	}
+}
+
+var_dump($coursePlayList);
+
+foreach($coursePlayList as $playUrl)
+{
+	curl_setopt($ch, CURLOPT_URL, $playUrl);
+	//打开一个页面
+	$tmp = 0;
+	
+	$playerPageHtml = curl_redirect_exec($ch, $tmp, true);
+	
+	$errorLogs = "";
+	while(true) {
+	
+		try {
+			// 开始发校验包
+			//reloadSession
+			$reloadSessionUrl = $baseUrl . "/Public/MessageNotice/ReloadSession";
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_URL, $reloadSessionUrl);
+			$output = curl_exec($ch);
+			//var_dump($output);
+			
+			//另外一个校验包
+			$requestUrl = GetRequestUrlByHtml($playerPageHtml);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_URL, $requestUrl);
+			$output = curl_exec($ch);
+			$json = str_replace( ")", "", str_replace("?(", "", $output));
+			
+			$result = json_decode($json, true);
+			//var_dump($result);
+			
+			if($result["State"]!=1) {
+				echo "\n========================================\n";
+				echo " 数据异常:校验包返回值是:\n";
+				echo "\nplayerPageHtml\n";
+				var_dump($playerPageHtml);
+				echo "\nresult\n";
+				var_dump($result);
+				echo "\nrequestUrl\n";
+				var_dump($requestUrl);
+				
+				echo "\n========================================\n";
+				//throw new Exception();
+				// 切换视频时有时候会出现空，只记录不再抛异常 
+				// 可能是在进程sleep的时候 正好看到了100% 
+				// 结果while没有跳出，导致数据异常，现在发现数据不正常就直接break去看下一个视频
+				break;
+				
+			} else {
+				echo "\n========================================\n";
+				echo " 完成进度 ".$result["Value"]["Process"]."%   " . date("Y-m-d H:i:s");
+				echo "\n========================================\n";
+				
+				if( $result["Value"]["Process"] >= 100) {	//进度完成，继续下一个
+					break;
+				}
+			}
+			
+			sleep(5);
+			
+		} catch ( Exception $e ) {
+			
+			echo "\n========================================\n";
+			echo "返回的数据包异常！！！";
+			echo "程序结束！！！";
+			echo "\n========================================\n";
+			break 2;
+		}
+	}
+}
+
+
+curl_close($ch);
+
+
+
+
+
+
+
+
+
+
